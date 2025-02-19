f11590e1435deb9cda15a4f327b65680c1b3af46
HD88-max <hardikdhallam@gmail.com>
2025-02-19 10:58:24 +0000

    Add files via upload

---
+++ agepass.py
@@ -0,0 +1,13 @@
+try:
+    age = int(input("Enter you age: "))
+    if age >= 18:
+        print("You have valid access.")
+    elif age < 18:
+        age2 = 18 -  age 
+        print(f"You are {age} years old.You have {age2} years left till valid access")
+    else:
+        raise ValueError
+except ValueError as h:
+    print("You have enter {0} please check your age and try again.".format(h))
+finally:
+    print("Enjoy no matter what lays ahead of you.")
