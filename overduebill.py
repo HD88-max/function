e481c121a13862bedc7978a4d7ffd7a3e139153c
HD88-max <hardikdhallam@gmail.com>
2025-02-19 00:31:01 +0000

    Add files via upload

--- overduebill.py
+++ overduebill.py
@@ -2,19 +2,18 @@ def total_bill(n):
     print("You need to pay {0}".format(n))
 def paid(y):
     print("You have paid {0}".format(y))
-y = int(input("How much have you paid"))
-n = int(input("How much is the total cost?"))
-print(total_bill(n))
-print(paid(y))
-if n > y:
-    tip = n - y
-    yn = input("Do you want to pay £",tip,end="(y for yes and n for no)?").upper()
-    if yn == "Y":
-        print("Thank you for your tip.")
-    elif yn == "N":
-        print("Ok have a great day.")  
+bill = int(input("How much is the total bill?"))
+payment = int(input("How much have you paid"))
+print(total_bill(bill))
+print(paid(payment))
+if bill > payment:
+    tip = payment-bill
+    balance=bill-payment
+    response = input("Do you want to pay tip (y for yes and n for no)?").upper()
+    if response == "Y":
+        print(f"Thank you for your tip {tip} ")
     else:
-        print("Say yes or no.")
+        print(f"Your balance is {balance}. have a great day.")  
 else:
-    overdue = y - n
+    overdue = bill - payment
     print("You have £",overdue,"to pay.")
