9cb0f7fb2b9f3e6bba7d97bed893e4cdb6e95698
HD88-max <hardikdhallam@gmail.com>
2025-02-20 10:44:06 +0000

    Delete mathoperations.py

--- mathoperations.py
+++
@@ -1,9 +0,0 @@
-import math
-number = float(input("Enter a decimal value: "))
-print(f"The floor value of {number} is {math.floor(number)}")
-print("The ceil value of {0} is {1}.".format(number,math.ceil(number)))
-x = float(input("Enter a value: "))
-y = float(input("Enter a negative value: "))
-print("The copysign of {0} and {1} is {2}.".format(x,y,math.copysign(x,y)))
-square = float(input("Enter a value: "))
-print("The sqaure root of {0} is {1}.".format(square,math.sqrt(square)))
